12d6d59a50cac1abd72f4b6996c08c3e9f46abbb
adgjllp <751109146@qq.com>
2020-02-12 15:18:08 +0800

    2020.2.12.15:17

--- src/main/java/com/review/java/controller/JsController.java
+++ src/main/java/com/review/java/controller/JsController.java
@@ -2,16 +2,12 @@ package com.review.java.controller;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class JsController {
-    @RequestMapping("/hello")
-    public String hello(@RequestParam(name = "name")String name ,Model model){
-        model.addAttribute(name);
-        return "helloWorld";
-
+    @RequestMapping("/")
+    public String hello(){
+        return "hello";
     }
 }
